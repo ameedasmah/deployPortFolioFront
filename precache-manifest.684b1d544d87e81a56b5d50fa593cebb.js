86625b89c69929dc711ed7c46d4245335c458f1f
ameedasmah <ameed.asmah@hotmail.com>
2021-03-18 01:55:53 +0200

    Updates

---
+++ precache-manifest.684b1d544d87e81a56b5d50fa593cebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db6bf47be90497c37ca83f14dd7cce1",
+    "url": "/deployPortFolioFront/index.html"
+  },
+  {
+    "revision": "b34d8a5e306f223453ef",
+    "url": "/deployPortFolioFront/static/css/main.a38b522e.chunk.css"
+  },
+  {
+    "revision": "dff87c12e57cb0d0d49d",
+    "url": "/deployPortFolioFront/static/js/2.3b8e274d.chunk.js"
+  },
+  {
+    "revision": "a1f11c6785a8be52b7a556dbf74a05ff",
+    "url": "/deployPortFolioFront/static/js/2.3b8e274d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34d8a5e306f223453ef",
+    "url": "/deployPortFolioFront/static/js/main.62ca5fad.chunk.js"
+  },
+  {
+    "revision": "994be3119ca5a79f651a",
+    "url": "/deployPortFolioFront/static/js/runtime-main.1acf7621.js"
+  }
+]);
